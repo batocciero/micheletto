e8dad3db2d94a8cfcccfa094907ad5bca50131dc
Felipe Batocciero <felipebatocciero@gmail.com>
2020-11-26 10:52:46 -0300

    Fixed the images rows on services

--- theme/servicos.php
+++ theme/servicos.php
@@ -55,15 +55,19 @@
         </p>
 
         <div class="row mb-4">
-            <div class="col-md-6">
-                <img class="img-fluid" src="<?= url("theme/img/servicos/s5.png"); ?>" alt="Vinhos Louveira">
+            <div class="col-md-4">
+                <img class="img-fluid" src="<?= url("theme/img/servicos/new-ciclistas-1.png"); ?>" alt="Vinhos Louveira">
             </div>
-            <div class="col-md-6">
+            <div class="col-md-4">
+                <img class="img-fluid mt-3 mt-md-0" src="<?= url("theme/img/servicos/s5.png"); ?>" alt="Vinhos Louveira">
+            </div>
+
+            <div class="col-md-4">
                 <img class="img-fluid mt-3 mt-md-0" src="<?= url("theme/img/servicos/s6.png"); ?>" alt="Vinhos Louveira">
             </div>
         </div>
 
-        <p class="text-wine font-weight-bolder title-size">
+        <p class="text-wine font-weight-bolder mt-5 mt-lg-0 title-size">
             ATRAÇÕES PARA CRIANÇAS
         </p>
 
@@ -90,7 +94,7 @@
         <p class="text-justify">
             Realize seu ensaio fotográfico aproveitando toda a paisagem natural, os vinhedos, as instalações, os animais e o encantador por do sol no Sítio Santa Rita - Vinícola Micheletto.
             <br>
-            <strong>Informações e reservas através do e-mail: </strong>
+            <strong>Informações através do e-mail: </strong>
             <span class="text-wine font-weight-bold">vinhosmicheletto@outlook.com</span>
         </p>
 
@@ -105,7 +109,7 @@
         <p class="text-justify">
             Recentemente criamos um espaço alternativo que nos dias e horários em que o Quiosque Micheletto não está funcionando poderá ser utilizado para confraternizações, aniversários, casamentos, cursos, palestras, treinamentos...
             <br>
-            <strong>Informações e reservas através do e-mail: </strong>
+            <strong>Informações através do e-mail: </strong>
             <span class="text-wine font-weight-bold">vinhosmicheletto@outlook.com</span>
         </p>
 
@@ -121,7 +125,7 @@
             <p class="text-justify">
                 O Sítio Santa Rita e a Vinícola Micheletto desenvolvem junto a seus parceiros permanentes testes na busca de melhorias para o processo da produção vitícola... Graças a essa constante busca pelo conhecimento nossos técnicos estão aptos a assessorar novos empreendedores e curiosos na implantação de vinhedos com variedades de uvas para mesa e/ou para processamento. Tem o desejo de implantar um vinhedo? Quer elaborar seu próprio vinho? Se você tem essas idéias e gostaria de desenvolvê-las pode contar conosco... Além do roteiro de Turismo Rural Técnico em Viticultura prestamos assessoria para implantação e condução de vinhedos...
                 <br> <br>
-                <strong>Informações e reservas através do telefone (19) 99732-5953</strong>
+                <strong>Informações através do telefone (19) 99732-5953</strong>
             </p>
         </div>
     </div>
