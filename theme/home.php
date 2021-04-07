bddb4daec3038f5480e408c7f9236544582a87c1
Felipe Batocciero <felipebatocciero@gmail.com>
2021-04-07 20:41:08 -0300

    Mudança no home.php Removido o conteúdo da pascoa. Adicionado a postagem (apenas texto) do conteúdo anterior que já estava no ar. Conteúdo sobre a seguda fase vermelha.

--- theme/home.php
+++ theme/home.php
@@ -14,19 +14,19 @@
   <div class="container-fluid px-md-5 ml-auto mr-auto mt-md-3">
     
     <!-- Pop Up com imagem -->
-    <div id="modal-addon"">
+    <!-- <div id="modal-addon"">
       <div class="modalconent d-grid bg-header text-center">
         <div class="txt-addon mx-4 text-justify" >Como de costume a Vinícola Micheletto estará fechada nesta Sexta-Feira Santa. 
         Convidamos nossos colaboradores, fornecedores, clientes e amigos a uma reflexão nesse tempo de tantas incertezas... 
         Mantendo as restrições de acesso e os procedimentos de higiene nosso Empório reabrirá no Sábado das 8h às 18h e Domingo 
         das 8h às 14h. Brindemos a Páscoa com Vinhos Micheletto!!!</div>
-        <img class="img-addons mr-auto ml-auto mt-2 mb-2" src=" <?= url("theme/img/addons/jesus.jpg"); ?> " alt="Estamos fechados para final de ano.">
+        <img class="img-addons mr-auto ml-auto mt-2 mb-2" src=" PATH TO IMAGE " alt="Estamos fechados para final de ano.">
         <button type="button" class="btn btn-margins btn-wine mt-2" id="button-addon">Fechar</button>
       </div>
-    </div>
+    </div> -->
 
     <!-- Pop Up apenas textos -->
-    <!-- <div id="modal-addon">
+    <div id="modal-addon">
       <div class="modalconent d-grid">
         <div class="container-text-addon">
           <p class="title-addon">
@@ -42,7 +42,7 @@
         </div>
         <button type="button" class="btn btn-wine mt-2" id="button-addon">Fechar</button>
       </div>
-    </div> -->
+    </div>
     <!-- End. Pop Up apenas textos -->
 
     <div class="row mx-0 px-md-5 text-center">
